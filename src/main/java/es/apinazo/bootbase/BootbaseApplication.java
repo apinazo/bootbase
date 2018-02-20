7e9284dee678247a69493713c3a55a9be2e2ff40
EXEVI\alpinazo <alpinazo@exevi.com>
2018-02-20 18:19:38 +0100

    Add Actuator, MVC and sample configurations.

--- src/main/java/es/apinazo/bootbase/BootbaseApplication.java
+++ src/main/java/es/apinazo/bootbase/BootbaseApplication.java
@@ -3,10 +3,14 @@ package es.apinazo.bootbase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// @SpringBootApplication equals to @Configuration, @EnableAutoConfiguration and @ComponentScan.
+// @ComponentScan will scan all packages here and below.
 @SpringBootApplication
 public class BootbaseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BootbaseApplication.class, args);
 	}
 }
