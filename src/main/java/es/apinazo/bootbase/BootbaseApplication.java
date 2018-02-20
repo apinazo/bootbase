2c50c8d0da51fec406b0e5f8cab415404f3c5fbd
EXEVI\alpinazo <alpinazo@exevi.com>
2018-02-20 15:37:35 +0100

    Import Spring Boot simple project

---
+++ src/main/java/es/apinazo/bootbase/BootbaseApplication.java
@@ -0,0 +1,12 @@
+package es.apinazo.bootbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootbaseApplication.class, args);
+	}
+}
---
+++ src/test/java/es/apinazo/bootbase/BootbaseApplicationTests.java
@@ -0,0 +1,16 @@
+package es.apinazo.bootbase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootbaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
