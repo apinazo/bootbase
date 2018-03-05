a17fc3ab31050612e5ccb6f0bea47229cb03a396
EXEVI\alpinazo <angelpinazo@gmail.com>
2018-03-05 18:07:04 +0100

    Fix discriminator.

--- src/main/java/es/apinazo/bootbase/business/persons/Cat.java
+++ src/main/java/es/apinazo/bootbase/business/persons/Cat.java
@@ -1,9 +1,17 @@
 package es.apinazo.bootbase.business.persons;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("1")
 public class Cat extends Pet {
+
+    public Cat(){}
 
     public Cat(String name, int age, Gender gender) {
         super(name, age, gender);
--- src/main/java/es/apinazo/bootbase/business/persons/Pet.java
+++ src/main/java/es/apinazo/bootbase/business/persons/Pet.java
@@ -1,5 +1,8 @@
 package es.apinazo.bootbase.business.persons;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -7,12 +10,21 @@ import lombok.ToString;
 
 import javax.persistence.*;
 
+// Tell Jackson to add a property named "type" to the JSON for Pet subclasses.
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+    // A Cat will have type="cat".
+    @JsonSubTypes.Type(value = Cat.class, name = "cat"),
+    // And a Dog, type="dog".
+    @JsonSubTypes.Type(value = Dog.class, name = "dog")
+})
 @Data
 @NoArgsConstructor
 @ToString(exclude = "person")
 @EqualsAndHashCode(exclude = "person")
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+// Integer discriminator is less clear but much more efficient than a String one.
 @DiscriminatorColumn(name = "kind", discriminatorType = DiscriminatorType.INTEGER)
 public abstract class Pet {
 
@@ -26,8 +38,10 @@ public abstract class Pet {
 
     private Gender gender;
 
+    @JsonIgnore // To break infinite recursion with Person.
     @ManyToOne(fetch = FetchType.LAZY)
     private Person person;
+
 
     public Pet(String name, int age, Gender gender) {
         super();
