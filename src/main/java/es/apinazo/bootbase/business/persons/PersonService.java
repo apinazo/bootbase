5f18291bb7f508035dfd9afaa07f7468498a5f17
EXEVI\alpinazo <angelpinazo@gmail.com>
2018-03-05 09:52:19 +0100

    Fix service pointcut.

--- src/main/java/es/apinazo/bootbase/business/persons/PersonService.java
+++ src/main/java/es/apinazo/bootbase/business/persons/PersonService.java
@@ -1,12 +1,22 @@
 package es.apinazo.bootbase.business.persons;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 @Slf4j
 @Service
 public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        Assert.notNull(personRepository, "personRepository");
+        this.personRepository = personRepository;
+    }
 
     public void doSomeMagic(){
         log.info("Doing magic !!!");
