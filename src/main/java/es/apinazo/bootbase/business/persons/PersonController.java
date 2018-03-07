37012fa56ef56993d42603f0069a0dbe552efb54
EXEVI\alpinazo <angelpinazo@gmail.com>
2018-03-07 10:58:37 +0100

    Add HTTP requests timing.

--- src/main/java/es/apinazo/bootbase/business/persons/PersonController.java
+++ src/main/java/es/apinazo/bootbase/business/persons/PersonController.java
@@ -1,5 +1,6 @@
 package es.apinazo.bootbase.business.persons;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
