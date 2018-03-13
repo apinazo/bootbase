08a2b5f17933e71920fbf84a2772452324eca8c0
EXEVI\alpinazo <angelpinazo@gmail.com>
2018-03-13 12:55:46 +0100

    Add example of stopping the app launch with an exception with a custom exit code.

---
+++ src/main/java/es/apinazo/bootbase/configuration/demo/ExitOnFailureConfiguration.java
@@ -0,0 +1,53 @@
+package es.apinazo.bootbase.configuration.demo;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Data
+@Slf4j
+@Configuration
+@ConfigurationProperties("configurations")
+public class ExitOnFailureConfiguration {
+
+    private boolean fail;
+
+    public static class CustomExitOnFailureException
+        extends Exception
+        implements ExitCodeGenerator {
+
+        /**
+         * Implementing {@link ExitCodeGenerator} you can define
+         * which exit code to use when this exception is thrown.
+         *
+         * @return The exit code.
+         */
+        @Override
+        public int getExitCode() {
+            return 666;
+        }
+    }
+
+    /**
+     * Maybe your app depends on a very important configuration
+     * or condition it can't work without.
+     * You can check it in {@link PostConstruct} and if so,
+     * stop the application at all.
+     *
+     * @throws CustomExitOnFailureException The exception.
+     */
+    @PostConstruct
+    public void check() throws CustomExitOnFailureException {
+
+        // If configurations.fail == true, stop the app
+        // with the custom exit code for this exception.
+        if (fail) {
+            throw new CustomExitOnFailureException();
+        }
+    }
+
+}
